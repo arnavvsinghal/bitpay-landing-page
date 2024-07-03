d09e2e1ba0d515c06efc6cfd087e378dfa4ded2d
Arnav <arnavsinghaldev@gmail.com>
2024-07-03 18:01:14 +0530

    feat: :sparkles: add stick-scroll heading

---
+++ src/components/sticky-scroll/StickyScrollHeading.tsx
@@ -0,0 +1,15 @@
+import { FunctionComponent } from "react";
+interface StickyScrollHeadingProps {
+  children: React.ReactNode;
+}
+const StickyScrollHeading: FunctionComponent<StickyScrollHeadingProps> = ({
+  children,
+}) => {
+  return (
+    <div className="text-8xl px-4 pt-4 text-center text-textprimary font-semibold">
+      {children}
+    </div>
+  );
+};
+
+export default StickyScrollHeading;
