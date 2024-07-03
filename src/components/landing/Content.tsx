cd042302dbc9914dfcc5dd7c3d821118f7280b38
Arnav <arnavsinghaldev@gmail.com>
2024-07-03 16:27:56 +0530

    feat: :sparkles: add landing page content component

---
+++ src/components/landing/Content.tsx
@@ -0,0 +1,32 @@
+import { FunctionComponent } from "react";
+import { motion } from "framer-motion";
+
+interface ContentComponentProps {
+  content: string;
+}
+
+const ContentComponent: FunctionComponent<ContentComponentProps> = () => {
+  return (
+    <>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1, delay: 1.5 }}
+        className="text-3xl pt-4 pb-1 font-normal text-texttertiary z-10"
+      >
+        Revolutionizing digital transactions, one bit at a time.
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 0.7 }}
+        transition={{ duration: 1, delay: 2.5 }}
+        className="text-lg text-texttertiary z-10"
+      >
+        Already have an account?
+        <button className="text-textprimary">&nbsp;Signin!</button>
+      </motion.div>
+    </>
+  );
+};
+
+export default ContentComponent;
