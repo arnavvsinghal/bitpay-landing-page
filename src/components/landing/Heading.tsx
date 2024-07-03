e9b8233f6efb8a7d7fb0acfe259a4f44cb2547cd
Arnav <arnavsinghaldev@gmail.com>
2024-07-03 16:25:08 +0530

    feat: :sparkles: add landing page heading component

---
+++ src/components/landing/Heading.tsx
@@ -0,0 +1,21 @@
+import { FunctionComponent } from "react";
+import { motion } from "framer-motion";
+
+interface HeadingComponentProps {
+  heading: string;
+}
+
+const HeadingComponent: FunctionComponent<HeadingComponentProps> = () => {
+  return (
+    <motion.div
+      initial={{ scale: 0, opacity: 0 }}
+      animate={{ scale: [0, 1.1, 1], opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="text-9xl py-4 font-medium text-textprimary z-10"
+    >
+      BitPay
+    </motion.div>
+  );
+};
+
+export default HeadingComponent;
